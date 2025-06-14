ef7148ba6dc884794019534dfeec5731fb1964ce
YinchuanHe <davidhe92@hotmail.com>
2025-06-13 22:43:23 -0700

    Add mongodb dependency and remove google font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PIV Club Game Planer",
@@ -19,7 +17,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
