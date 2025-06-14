a5a098f05524315b3e24d69adb3d7eb973b690d3
yinchuan <davidhe92@hotmail.com>
2025-06-14 12:05:56 -0700

    fix google auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { google } from 'better-auth/social-providers';
-import { nextCookies, toNextJsHandler } from 'better-auth/integrations/next-js';
+import { nextCookies, toNextJsHandler } from 'better-auth/next-js';
 import { getClient } from '../utils/db';
 
 let handler: ReturnType<typeof toNextJsHandler> | null = null;
@@ -11,12 +11,12 @@ async function getHandler() {
     const client = await getClient();
     const auth = betterAuth({
       adapter: mongodbAdapter(client.db()),
-      socialProviders: [
-        google({
-          clientId: process.env.GOOGLE_CLIENT_ID || '',
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-        }),
-      ],
+      socialProviders: {
+        google:{
+          clientId: process.env.GOOGLE_CLIENT_ID as string,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
       plugins: [nextCookies()],
     });
     handler = toNextJsHandler(auth);
