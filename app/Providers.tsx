a5b48a622fc3c2e7da34169c9a26037cf60f7bd0
yinchuan <davidhe92@hotmail.com>
2025-06-14 20:56:48 -0700

    add back app bar

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,8 +1,14 @@
 'use client'
 
+import AppBar from '@/components/AppBar'
 import { SessionProvider } from 'next-auth/react'
 import type { ReactNode } from 'react'
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <SessionProvider>
+      <AppBar/>
+      {children}
+    </SessionProvider>
+  );
 }
