cfe825362fc81379c41ec56f4910a64ca960c005
YinchuanHe <davidhe92@hotmail.com>
2025-06-21 15:50:54 -0700

    Add footer navigation

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import AppBar from '@/components/AppBar'
+import Footer from '@/components/Footer'
 import { SessionProvider } from 'next-auth/react'
 import type { ReactNode } from 'react'
 
@@ -9,7 +10,8 @@ export function Providers({ children }: { children: ReactNode }) {
     <SessionProvider>
       <div className="flex flex-col min-h-dvh">
         <AppBar />
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow pb-16">{children}</main>
+        <Footer />
       </div>
     </SessionProvider>
   );
