e543d2569c36a5b56585a58560b9466f92cb85a5
YinchuanHe <yhe023@ucr.edu>
2025-06-20 22:12:48 -0700

    fix mobile viewport (#61)

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -7,7 +7,7 @@ import type { ReactNode } from 'react'
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-dvh">
         <AppBar />
         <main className="flex-grow">{children}</main>
       </div>
